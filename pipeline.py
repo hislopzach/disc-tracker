dbb84dc7294e860131e73fbe770e5521570808b7
Zach Hislop <zach@praevium.com>
2020-11-20 13:55:37 -0800

    use knn background remover. clean fg mask with connected components

--- pipeline.py
+++ pipeline.py
@@ -51,6 +51,111 @@ def show_removed_background(video_filename, background_image):
             cv.waitKey(5)
 
 
+def show_knn_removed_background(video_filename):
+    cap = cv.VideoCapture(video_filename)
+    videoWidth = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    videoHeight = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    frame_rate = cap.get(5)
+
+    back_sub = cv.createBackgroundSubtractorKNN()
+    summed_mask = np.zeros((videoHeight, videoWidth)).astype("uint8")
+    totalFrames = cap.get(cv.CAP_PROP_FRAME_COUNT)
+
+    start_overlay = False
+    for i in range(int(totalFrames)):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if not start_overlay:
+            cv.imshow("frame", frame)
+            key = cv.waitKey()
+            if key == ord("a"):
+                start_overlay = True
+        if ret:
+            fgMask = back_sub.apply(frame)
+            if start_overlay:
+                # eroded_mask = erode_and_dilate(fgMask, (5, 5))
+                cleaned_mask = remove_small_components(fgMask, 20)
+                cleaned_mask = remove_large_components(cleaned_mask, 1000)
+                # cleaned_mask = keep_medium_components(fgMask)
+                # isolated_mask = remove_lone_pixels(eroded_mask)
+                summed_mask += cleaned_mask
+                result_frame = add_overlay(frame, summed_mask, 0.8)
+            else:
+                result_frame = frame
+            cv.imshow("result frame", result_frame)
+            cv.waitKey(5)
+
+
+def remove_lone_pixels(image):
+    image = image.copy()
+    image_comp = cv.bitwise_not(image)  # could just use 255-image
+
+    kernel1 = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]], np.uint8)
+    kernel2 = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]], np.uint8)
+
+    hitormiss1 = cv.morphologyEx(image, cv.MORPH_ERODE, kernel1)
+    hitormiss2 = cv.morphologyEx(image_comp, cv.MORPH_ERODE, kernel2)
+    hitormiss = cv.bitwise_and(hitormiss1, hitormiss2)
+
+    hitormiss_comp = cv.bitwise_not(hitormiss)  # could just use 255-img
+    result = cv.bitwise_and(image, image, mask=hitormiss_comp)
+    return result
+
+
+def remove_small_components(image, min_size=25):
+    # find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(image, connectivity=8)
+    # connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    # the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
+    sizes = stats[1:, -1]
+    nb_components = nb_components - 1
+
+    # your answer image
+    result = np.zeros((output.shape))
+    # for every component in the image, you keep it only if it's above min_size
+    for i in range(0, nb_components):
+        if sizes[i] >= min_size:
+            result[output == i + 1] = 255
+
+    return result.astype("uint8")
+
+
+def remove_large_components(image, max_size=150):
+    # find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(image, connectivity=8)
+    # connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    # the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
+    sizes = stats[1:, -1]
+    nb_components = nb_components - 1
+
+    # your answer image
+    result = np.zeros((output.shape))
+    # for every component in the image, you keep it only if it's above min_size
+    for i in range(0, nb_components):
+        if sizes[i] <= max_size:
+            result[output == i + 1] = 255
+
+    return result.astype("uint8")
+
+
+def keep_medium_components(image, min_size=25, max_size=500):
+    # find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(image, connectivity=8)
+    # connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    # the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
+    sizes = stats[1:, -1]
+    nb_components = nb_components - 1
+
+    # your answer image
+    result = np.zeros((output.shape))
+    # for every component in the image, you keep it only if it's above min_size
+    for i in range(0, nb_components):
+        if sizes[i] <= max_size or sizes[i] >= min_size:
+            result[output == i + 1] = 255
+
+    return result.astype("uint8")
+
+
 def highlight_movement(frame, background_image, summed_mask):
     video_width = frame.shape[1]
     video_height = frame.shape[0]
@@ -80,11 +185,19 @@ def add_overlay(frame, mask, alpha):
     return res
 
 
+def erode_and_dilate(img, kernel=(3, 3)):
+    img = img.copy()
+    img = cv.erode(img, kernel)
+    img = cv.dilate(img, kernel)
+    return img
+
+
 def main():
     video_filename = sys.argv[1]
-    background_image = get_background_image(video_filename)
+    # background_image = get_background_image(video_filename)
 
-    show_removed_background(video_filename, background_image)
+    # show_removed_background(video_filename, background_image)
+    show_knn_removed_background(video_filename)
 
 
 if __name__ == "__main__":
